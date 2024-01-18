bee2eccccbd680fdb194186a496d9d4b01266cc9
Corey <Cordog_8381@outlook.com>
2024-01-18 16:47:22 -0500

    init

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Header from './components/Header'
+
+
+function App() {
+
+  return (
+    <>
+    <Header/>
+      <div className='center text-center'> Hello World</div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+
+  return (
+    <>
+      <div className='center text-center'> Hello Header</div>
+    </>
+  )
+}
+
+export default Header
