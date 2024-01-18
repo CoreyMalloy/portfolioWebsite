9f222fc1634c678c363e46b4c2b3396816a159c8
Corey <Cordog_8381@outlook.com>
2024-01-18 17:12:51 -0500

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import './App.css'
-import Header from './components/Header'
-
 
 function App() {
 
   return (
     <>
-    <Header/>
-      <div className='center text-center'> Hello Vercel</div>
+      <div className='flex items-center justify-center'>
+        <div className='text-center w-1/2'>
+          <h1 className='text-3xl'>Hi, Im Corey Malloy</h1>
+          <p>Hello and welcome to my website</p>
+          <ul>
+            <li>student</li>
+            <li>designer</li>
+            <li>programmer</li>
+          </ul>
+          <p>My name is Corey, and I am a Senior Student located in Cleveland Ohio. Aside from doing schoolwork, I work on web projects, ride my bike, and tinker around on my computer!</p>
+        </div>
+      </div>
     </>
   )
 }
