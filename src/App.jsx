d45b5a0e30bf3617c0fbb21e246a1d5447f76836
Corey <Cordog_8381@outlook.com>
2024-01-24 13:45:52 -0500

    minor change

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,22 @@ function App() {
     <>
       <div className='flex items-center justify-center'>
         <div className='text-left w-1/2 margin'>
-          <h1 className='text-3l'><b>Corey Malloy</b></h1>
-          <p>Hello and welcome to my website</p>
-          <ul>
-            <li>student</li>
-            <li>designer</li>
-            <li>programmer</li>
-          </ul>
+          <h1 className='mb-7 text-3l'><b>Corey Malloy</b></h1>
+          <p className=''>Hello and welcome to my website</p>
+          <p>In the past I have worked under a web development entrepreneur to enhance my web development skills</p>
+          <div>
+            <ul>
+              <li>student</li>
+              <li>designer</li>
+              <li>programmer</li>
+            </ul>
+          </div>
+        </div>
+        
+      </div>
+      <div className='flex items-center justify-center'>
+        <div className='text-left w-1/2 margin'>
+          <h1><b>Now</b></h1>
           <p>My name is Corey, and I am a Senior Student located in Cleveland Ohio. Aside from doing schoolwork, I work on web projects, ride my bike, and tinker around on my computer!</p>
         </div>
       </div>
